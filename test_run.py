0478f2e25665664df03fee0349e28c7b4f3758f1
Michael <hoppes@gmail.com>
2025-05-06 16:11:49 -0600

    Add future test cases

--- test_run.py
+++ test_run.py
@@ -11,6 +11,9 @@ class TestDateDiff:
         assert datediff.DateDiff.today_diff('2025-01-01', TODAY) == {'years': 0, 'months': 4, 'days': 124}
         assert datediff.DateDiff.today_diff('2025-01-02', TODAY) == {'years': 0, 'months': 4, 'days': 123}
         assert datediff.DateDiff.today_diff('2020-01-01', TODAY) == {'years': 5, 'months': 64, 'days': 1951}
+        assert datediff.DateDiff.today_diff('2025-05-06', TODAY) == {'years': 0, 'months': 0, 'days': -1}
+        assert datediff.DateDiff.today_diff('2025-06-05', TODAY) == {'years': 0, 'months': -1, 'days': -31}
+        assert datediff.DateDiff.today_diff('2026-05-05', TODAY) == {'years': -1, 'months': -12, 'days': -365}
 
     def test_read_file(self):
         with open(datediff.DateDiff.INPUT_FILE, 'w') as f:
